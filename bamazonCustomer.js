34ce45ecf1148ba235be144acde58336fd762489
AZCoder018 <jason.meadows.esq@hotmail.com>
2018-04-27 16:56:43 -0700

    establish database connection

---
+++ bamazonCustomer.js
@@ -0,0 +1,20 @@
+//Require mySql and Inquirer
+var mySql = require("mysql");
+var inquirer = require("inquirer");
+
+//Connection to SQL database
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "DrPepper",
+    database: "Bamazon"
+});
+
+//Confirm connection to SQL database
+connection.connect(function(err){
+    if (err) throw err; 
+    console.log("connected as id: " + connection.threadid)
+});
+
+//Display all available items in store
